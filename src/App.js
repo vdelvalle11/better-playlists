064add3fe2998470aaec9c0efd0c4afda55279cd
Victor Del Valle <vdelvalle11@gmail.com>
2018-12-20 19:33:52 -0500

    The start of learning react!

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    let firstName = 'Victor'
+    let lastName = 'Del Valle'
+    let red = '#FF1212'
+    let nameStyle = {color: red}
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            My name is <span style={nameStyle}>{firstName} {lastName}</span> and I am learning react!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
